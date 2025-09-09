427e9195d00ff5e4eeda76fc9cd40cc846f3224f
lysolaka <lysolaka@gmail.com>
2025-09-09 20:10:33 +0200

    Revision 6: switch to transparency full-time

--- lua/config/plugins/ui.lua
+++ lua/config/plugins/ui.lua
@@ -1,8 +1,8 @@
 return {
   ["tokyonight"] = {
-    transparent = false,
+    transparent = true,
     styles = {
-      -- sidebars = "transparent",
+      sidebars = "transparent",
       -- floats = "transparent",
     },
     style = "night",

e198c35ca4935dc8961283fcce4d42ed26c00a23
lysolaka <lysolaka@gmail.com>
2025-10-12 15:48:04 +0200

    Revision 7: make an auxilary files directory for latex documents

--- lua/plugins/tex.lua
+++ lua/plugins/tex.lua
@@ -3,6 +3,11 @@ return {
     "lervag/vimtex",
     ft = "tex",
     init = function()
+      vim.g.vimtex_compiler_latexmk = {
+        aux_dir = function(file_info)
+          return file_info.target_name .. ".auxdir"
+        end,
+      }
       vim.g.vimtex_compiler_latexmk_engines = {
         _ = "-pdf --shell-escape",
         pdflatex = "-pdf --shell-escape",

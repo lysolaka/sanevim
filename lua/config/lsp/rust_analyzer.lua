a13b790df590344baada2ae552a7f992da3f73ca
lysolaka <lysolaka@gmail.com>
2025-11-21 18:38:37 +0100

    Revision 8: reconfigure `rust_analyzer`

--- lua/config/lsp/rust_analyzer.lua
+++ lua/config/lsp/rust_analyzer.lua
@@ -1,10 +1,18 @@
 return {
   settings = {
     ["rust-analyzer"] = {
+      cargo = {
+        buildScripts = {
+          enable = true
+        },
+      },
       completion = {
         autoimport = {
           enable = false
         },
+      },
+      procMacro = {
+        enable = true
       },
     },
   },

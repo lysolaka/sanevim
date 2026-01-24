79ca3d39e42423c1538a683a70d67a6acd5e0c9f
lysolaka <lysolaka@gmail.com>
2026-01-24 09:30:14 +0100

    feat: enable all configs

---
+++ init.lua
@@ -0,0 +1,12 @@
+-- General nvim Configuration --
+require("config.nvim")
+-- Setup lazy.nvim --
+require("config.lazy")
+-- UI Configuration --
+require("config.ui")
+-- LSP Configuration --
+require("config.lsp")
+-- Treesitter Configuration --
+require("config.treesitter")
+-- Keybindings --
+require("config.keybinds")

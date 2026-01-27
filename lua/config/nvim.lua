0b7b2c0b59e8662c4f53678d7f7975b511ec67f5
lysolaka <lysolaka@gmail.com>
2026-01-27 15:32:12 +0100

    refactor: replace `vim.opt` with `vim.o`

--- lua/config/nvim.lua
+++ lua/config/nvim.lua
@@ -3,12 +3,12 @@
 vim.g["mapleader"] = " "
 vim.g["maplocalleader"] = "\\"
 
-vim.opt["termguicolors"] = true
-vim.opt["autoindent"] = true
-vim.opt["smartindent"] = true
-vim.opt["tabstop"] = 2
-vim.opt["shiftwidth"] = 2
-vim.opt["expandtab"] = true
-vim.opt["number"] = true
-vim.opt["signcolumn"] = "yes:1"
-vim.opt["clipboard"] = "unnamedplus,unnamed"
+vim.o["termguicolors"] = true
+vim.o["autoindent"] = true
+vim.o["smartindent"] = true
+vim.o["tabstop"] = 2
+vim.o["shiftwidth"] = 2
+vim.o["expandtab"] = true
+vim.o["number"] = true
+vim.o["signcolumn"] = "yes:1"
+vim.o["clipboard"] = "unnamedplus,unnamed"

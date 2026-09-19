56d904d1ba30437602f0beb5602de4da75023240
lysolaka <lysolaka@gmail.com>
2026-09-19 12:54:10 +0200

    fix: override rust-analyzer with the one installed using rustup

--- lua/config/lsp/rust_analyzer.lua
+++ lua/config/lsp/rust_analyzer.lua
@@ -1,6 +1,19 @@
 -- rust_analyzer Configuration --
 
+-- override rust-analyzer with the one installed using rustup, if it exists
+local function rust_analyzer()
+  local cargo_home = vim.env["CARGO_HOME"] or vim.fs.joinpath(vim.env["HOME"], ".cargo")
+  local ra = vim.fs.joinpath(cargo_home, "bin", "rust-analyzer")
+
+  if vim.fn.executable(ra) == 1 then
+    return ra
+  else
+    return "rust-analyzer"
+  end
+end
+
 return {
+  cmd = { rust_analyzer() },
   settings = {
     ["rust-analyzer"] = {
       cargo = {
